a028fbae01880c5a2e74883996606c4fe2b0f9f6
Nigel <nigeldcruz7@gmail.com>
2017-05-06 11:49:10 +0530

    Navigation bar animated Perfectly

--- js/scroll.js
+++ js/scroll.js
@@ -27,3 +27,29 @@ $( document ).ready(function() {
   });
 
 
+
+
+$( document ).ready(function() {
+ 
+		
+	$(window).scroll(function() {
+		
+		if ($(document).scrollTop() > 50){
+			$('.head-nav .navbar-default').css({"min-height": "50px"});
+			$('.navbar-brand>img').css({"top":"10px"});
+			$('.navbar-nav').css({"top":"10px"});
+		}
+		
+		else{
+			
+			$('.head-nav .navbar-default').css({"min-height": "100px"});
+			$('.navbar-brand>img').css({"top":"45px"});
+			$('.navbar-nav').css({"top":"40px"});
+			
+		}
+		
+		
+	});
+	
+
+  });
